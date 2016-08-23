615a1be2d856ba759be442570da335c712b412ef
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2016-08-23 15:47:43 +0900

    [PATCH 7/11] Add JSON validation to aws_kms_key.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- builtin/providers/aws/resource_aws_kms_key.go
+++ builtin/providers/aws/resource_aws_kms_key.go
@@ -55,6 +55,8 @@ func resourceAwsKmsKey() *schema.Resource {
 				Type:             schema.TypeString,
 				Optional:         true,
 				Computed:         true,
+				StateFunc:        normalizeJsonStateFunc,
+				ValidateFunc:     validateJsonFunc,
 				DiffSuppressFunc: suppressEquivalentAwsPolicyDiffs,
 			},
 			"is_enabled": &schema.Schema{
@@ -143,7 +145,8 @@ func resourceAwsKmsKeyRead(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	d.Set("policy", normalizeJson(*p.Policy))
+	policy, _ := normalizeJson(*p.Policy)
+	d.Set("policy", policy)
 
 	krs, err := conn.GetKeyRotationStatus(&kms.GetKeyRotationStatusInput{
 		KeyId: metadata.KeyId,
@@ -216,14 +219,14 @@ func resourceAwsKmsKeyDescriptionUpdate(conn *kms.KMS, d *schema.ResourceData) e
 }
 
 func resourceAwsKmsKeyPolicyUpdate(conn *kms.KMS, d *schema.ResourceData) error {
-	policy := d.Get("policy").(string)
+	policy, _ := normalizeJson(d.Get("policy").(string))
 	keyId := d.Get("key_id").(string)
 
 	log.Printf("[DEBUG] KMS key: %s, update policy: %s", keyId, policy)
 
 	req := &kms.PutKeyPolicyInput{
 		KeyId:      aws.String(keyId),
-		Policy:     aws.String(normalizeJson(policy)),
+		Policy:     aws.String(policy),
 		PolicyName: aws.String("default"),
 	}
 	_, err := conn.PutKeyPolicy(req)
