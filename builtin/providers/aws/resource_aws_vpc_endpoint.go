e34e27d589d72031fb677bcfecb5f01c144a3fbf
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2016-08-23 15:48:21 +0900

    [PATCH 10/11] Add JSON validation to aws_vpc_endpoint.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- builtin/providers/aws/resource_aws_vpc_endpoint.go
+++ builtin/providers/aws/resource_aws_vpc_endpoint.go
@@ -22,10 +22,11 @@ func resourceAwsVpcEndpoint() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"policy": &schema.Schema{
-				Type:      schema.TypeString,
-				Optional:  true,
-				Computed:  true,
-				StateFunc: normalizeJson,
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				StateFunc:    normalizeJsonStateFunc,
+				ValidateFunc: validateJsonFunc,
 			},
 			"vpc_id": &schema.Schema{
 				Type:     schema.TypeString,
@@ -60,7 +61,7 @@ func resourceAwsVPCEndpointCreate(d *schema.ResourceData, meta interface{}) erro
 	}
 
 	if v, ok := d.GetOk("policy"); ok {
-		policy := normalizeJson(v)
+		policy, _ := normalizeJson(v)
 		input.PolicyDocument = aws.String(policy)
 	}
 
@@ -128,8 +129,10 @@ func resourceAwsVPCEndpointRead(d *schema.ResourceData, meta interface{}) error 
 		return fmt.Errorf("There are multiple prefix lists associated with the service name '%s'. Unexpected", prefixListServiceName)
 	}
 
+	policy, _ := normalizeJson(*vpce.PolicyDocument)
+
 	d.Set("vpc_id", vpce.VpcId)
-	d.Set("policy", normalizeJson(*vpce.PolicyDocument))
+	d.Set("policy", policy)
 	d.Set("service_name", vpce.ServiceName)
 	if err := d.Set("route_table_ids", aws.StringValueSlice(vpce.RouteTableIds)); err != nil {
 		return err
@@ -162,7 +165,7 @@ func resourceAwsVPCEndpointUpdate(d *schema.ResourceData, meta interface{}) erro
 	}
 
 	if d.HasChange("policy") {
-		policy := normalizeJson(d.Get("policy"))
+		policy, _ := normalizeJson(d.Get("policy"))
 		input.PolicyDocument = aws.String(policy)
 	}
 
